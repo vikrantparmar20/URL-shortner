389c31a3423f1520c2ac154a66d924722171fddf
vikrantparmar20 <47989070+vikrantparmar20@users.noreply.github.com>
2025-06-08 17:23:40 +0530

    jwt auth and spring security config done

--- server/src/main/java/com/url/shortner/models/User.java
+++ server/src/main/java/com/url/shortner/models/User.java
@@ -16,5 +16,6 @@ public class User {
 	private Long id;
 	private String userName;
 	private String password;
+	private String email;
 	private String role="ROLE_USER";
 }
---
+++ server/src/main/java/com/url/shortner/service/UserDetailsImpl.java
@@ -0,0 +1,73 @@
+package com.url.shortner.service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.url.shortner.models.User;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserDetailsImpl implements UserDetails {/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String userName;
+	private String email;
+	
+	private String password;
+	
+	private Collection<? extends GrantedAuthority> authorities;
+	
+	public static UserDetailsImpl build(User user) {
+		//Spring security needs grantedAuthtority object to understand the role for 
+		//user object
+		GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+		return new UserDetailsImpl(
+				user.getId(),
+				user.getUserName(),
+				user.getEmail(),
+				user.getPassword(),
+				Collections.singletonList(authority)
+				);
+	}
+	
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return authorities;
+	}
+
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return userName;
+	}
+
+
+	public UserDetailsImpl(Long id, String userName, String email, String password,
+			Collection<? extends GrantedAuthority> authorities) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+		this.authorities = authorities;
+	}
+	
+}
